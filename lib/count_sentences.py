27a31577fb3d939d3349a8d7679e4365e2bab6ec
cindy kivala <cindykivala@gmail.com>
2025-08-09 14:56:05 +0300

    sentences exclamations logic

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,42 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value=''):
+    self.value = value
+    self.value = value
+
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self, new_value):
+      if isinstance(new_value, str):
+          self._value = new_value
+      else:
+          print("The value must be a string.")
+
+  def __str__(self):
+    return self.value
+  
+  def __len__(self):
+    return len(self.value)
+  
+  def is_sentence(self):
+    return self.value.endswith(('.', '!', '?'))
+  
+  def is_question(self):
+    return self.value.endswith('?')
+  
+  def is_exclamation(self):
+    return self.value.endswith('!')
+  
+  def count_sentences(self):
+    if not self.value:
+      return 0
+    sentences = self.value.split()
+    count = 0
+    for sentence in sentences:
+      if sentence.endswith(('.', '!', '?')):
+        count += 1
+    return count
